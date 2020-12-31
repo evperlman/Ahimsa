590edeab33d26eb377ef6bf3b181256a115db5ee
Will <whbladon@gmail.com>
2020-12-30 20:57:37 -0800

    working on controllers

--- server/controllers/bcryptController.js
+++ server/controllers/bcryptController.js
@@ -1,58 +1,65 @@
-const { Client } = require('pg');
-const database = new Client('postgres://cjcfvoek:7uijjLGHc90B-ztkZPf4p3xdYgjHBlvO@suleiman.db.elephantsql.com:5432/cjcfvoek');
-database.connect()
+const { Client } = require("pg");
+const database = new Client(process.env.DB_URL);
+database.connect();
 
-
-
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
 const bcryptController = {};
 
-//Hash the password and store username, hash 
-bcryptController.createPassword = (request, response, next) => {
-  const { password, username } = request.body;
-  bcrypt.hash(password, saltRounds, function(err, hash) {
-    if (err) { 
+/**
+ * @desc    Hashes the password and stores username, and hash in req.body
+ * @route   POST /register_user
+ */
+bcryptController.hashPassword = (request, response, next) => {
+  const { password, email, first_name, last_name } = request.body;
+  bcrypt.hash(password, saltRounds, function (err, hash) {
+    if (err) {
       return next(err);
     }
-    request.body = { hash, username,}
+    request.body = { hash, email, first_name, last_name };
     return next();
   });
 };
 
-//checks password input against the database and returns a boolean 
-bcryptController.checkPassword = (request, response, next) => {
-  console.log('getting username')
-  const { password, username } = request.body;
-  const text = `SELECT * FROM bc_storage WHERE username='${username}'`;
-  let data;
-    database.query(text, (err, res) => {
+/**
+ * @desc    Registers the username and hashed password into DB.
+ * @route   POST /register_user
+ */
+bcryptController.createUser = (request, response, next) => {
+  const { hash, email, first_name, last_name } = request.body;
+  const text = `INSERT INTO users (first_name, last_name, email, hash) VALUES ('${first_name}', '${last_name}','${email}', '${hash}');`;
+  database.query(text, (err, res) => {
     if (err) {
-      return next(err)
+      return next(err);
     } else {
-      data = res;
-      console.log('comparing passwords')
-      bcrypt.compare(password, data.rows[0].hash, function(err, res) {
+      console.log("Registered new user successfully.");
+      return next();
+    }
+  });
+};
+
+//checks password input against the database and returns a boolean
+/**
+ * @desc    Credential validation: compares hashed pw with input pw.
+ * @route   POST /check_pw
+ */
+bcryptController.checkPassword = (request, response, next) => {
+  console.log("getting username");
+  const { password, email } = request.body;
+  const text = `SELECT * FROM users WHERE email='${email}'`;
+  
+  database.query(text, (err, res) => {
+    if (err) next(err);
+    else {
+      if(!res.rows[0]) return next();
+      console.log("Comparing passwords...");
+      bcrypt.compare(password, res.rows[0].hash, function (err, res) {
         if (err) return next(err);
         response.locals.result = res;
         return next();
       });
-    };
-  });
-};
-
-//Stores encrypted password with username in db. 
-bcryptController.storeUserCredentials = (request, response, next) => {
-  const { username, hash } = request.body;
-  const text = `INSERT INTO bc_storage (username, hash) VALUES ('${username}', '${hash}');`;
-  database.query(text, (err, res) => {
-    if (err) {
-      return next(err)
-    } else {
-      console.log('completed query');
-      return next();
-    };
+    }
   });
 };
 
