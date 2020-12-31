9ccfbedbb951ce0aae7e6c3d8403d5304f5de0cb
evperlman <evperlman@gmail.com>
2020-12-31 12:34:24 -0800

    up to date

--- client/components/DisplayData.jsx
+++ client/components/DisplayData.jsx
@@ -1,16 +1,5 @@
 import React, { useState, useEffect } from 'react'; 
-// import Paper from '@material-ui/core/Paper';
-// import {
-//   Chart,
-//   BarSeries,
-//   Title,
-//   ArgumentAxis,
-//   ValueAxis,
-// } from '@devexpress/dx-react-chart-material-ui';     ---> stretch 
 
-// import { Animation } from '@devexpress/dx-react-chart';
-
-// Do visX stuff here 
 
 const DisplayData = () => {
   return (
